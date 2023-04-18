1b0d961437682c854ccb940a1519a3cc99530c4a
= <MaxsoftLtd>
2023-04-18 19:45:21 +0300

    Updated base url

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,12 +3,12 @@ import { tokenStorage } from "~/services/localStorage";
 
 const token = tokenStorage.get();
 
-// const baseURL = import.meta.env.DEV
-// 	? import.meta.env.VITE_DEV
-// 	: import.meta.env.VITE_PROD;
+const baseURL = import.meta.env.DEV
+	? `${import.meta.env.VITE_DEV}`
+	: `${import.meta.env.VITE_PROD}`;
 
 export default axios.create({
-	baseURL: `${import.meta.env.VITE_PROD}`,
+	baseURL,
 	headers: {
 		"Content-type": "application/json",
 		token,
