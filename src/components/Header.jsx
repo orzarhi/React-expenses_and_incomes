6e07e937cdc724ae3293530d2c1a4721c5e5973f
= <MaxsoftLtd>
2023-05-07 20:49:01 +0300

    Display change and fixed bugs

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,21 +7,21 @@ export const Header = () => {
 
 	return (
 		<header>
-			<nav className="pt-3 pr-5 text-lg sm:text-base sm:pr-0 sm:ml-5">
-				<ul className="flex justify-start max-w-screen-xl p-5 ml-5 text-white">
-					<li className="ml-8 sm:ml-5">
+			<nav className="pt-3 pr-5 text-lg sm:text-base sm:pr-0">
+				<ul className="flex justify-start max-w-screen-xl p-5 ml-5 text-white gap-x-12 lg:gap-x-8 md:gap-x-6 sm:gap-x-4 sm:p-2">
+					<li>
 						<Link to="/">בית</Link>
 					</li>
-					<li className="ml-8 sm:ml-5">
+					<li>
 						<Link to="/expenses">הוצאות</Link>
 					</li>
-					<li className="ml-8 sm:ml-5">
+					<li>
 						<Link to="/incomes">הכנסות</Link>
 					</li>
-					<li className="ml-8 sm:ml-5">
+					<li>
 						<Link to="/statistics">מה הלאה?</Link>
 					</li>
-					<li className="absolute left-0 ml-10 sm:ml-5">
+					<li className="absolute left-0 ml-10 sm:ml-3">
 						<button onClick={() => authCtx.logout()}>יציאה</button>
 					</li>
 				</ul>
